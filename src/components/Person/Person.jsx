e19b162922cd76515874040edbb47b8627f22f38
Bartek <kowalski_1410@wp.pl>
2024-05-21 22:23:39 +0200

    task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person = {} }) => {
+  let isAge = null;
+  let marriedPargraph = <p className="Person__partner">I am not married</p>
+  if('age' in person) {
+    isAge = <p className="Person__age">I am {person.age}</p>
+  }
+
+  if(person.isMarried) {
+    if(person.sex === 'm') {
+      marriedPargraph = <p className="Person__partner">{person.partnerName} is my wife</p>
+    } else {
+      marriedPargraph = <p className="Person__partner">{person.partnerName} is my husband</p>
+    }
+  }
+
+  return (
+    <section>
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {isAge}
+      {marriedPargraph}
+    </section>
+  )
+ };
