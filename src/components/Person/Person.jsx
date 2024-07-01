0e8f7ead628bca332bd7e5728f1bcfc24337f745
Bartek <kowalski_1410@wp.pl>
2024-07-01 20:41:48 +0200

    correct solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -14,7 +14,7 @@ export const Person = ({ person = {} }) => {
   }
 
   return (
-    <section>
+    <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
       {'age' in person && <p className="Person__age">I am {person.age}</p>}
       {marriedPargraph}
